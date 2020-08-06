eb4478bdd0d2828d44eb994bfdb451f66f5c652e
daff <daffmg1@gmail.com>
2020-08-06 08:14:06 -0500

    modal para registrar datos desde un componente a firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AngularFireModule } from '@angular/fire';
@@ -7,17 +8,22 @@ import { AngularFireDatabaseModule } from '@angular/fire/database';
 import { environment } from 'src/environments/environment';
 import { PlayerService } from './services/player.service';
 import { TeamTableComponent } from './team-table/team-table.component';
+import { PlayerTableComponent } from './player-table/player-table.component';
+import { PlayerDialogComponent } from './player-dialog/player-dialog.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TeamTableComponent
+    TeamTableComponent,
+    PlayerTableComponent,
+    PlayerDialogComponent
   ],
   imports: [
     BrowserModule,
     AngularFireDatabaseModule,
     AngularFireModule.initializeApp(environment.firebase),
     AppRoutingModule,
+    FormsModule
     
     
   ],
---
+++ src/app/player-dialog/player-dialog.component.ts
@@ -0,0 +1,58 @@
+import { Component, OnInit } from '@angular/core';
+import { Country, SquadNumber } from '../interfaces/player';
+import { PlayerService } from '../services/player.service';
+import { TeamService } from '../services/team.service';
+import { take } from 'rxjs/operators';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-player-dialog',
+  templateUrl: './player-dialog.component.html',
+  styleUrls: ['./player-dialog.component.scss']
+})
+export class PlayerDialogComponent implements OnInit {
+  private team;
+  public player;
+  public countries= Object.keys(Country).map(key => ({label:key,key:Country[key]}))
+  public squadNumber = Object.keys(SquadNumber)
+  .slice(Object.keys(SquadNumber).length/2)
+  .map(key => ({
+    label:key,
+    key: SquadNumber[key]
+  }))
+  constructor(private playerService :PlayerService, private teamService:TeamService) { }
+
+  ngOnInit(): void {
+    this.teamService
+    .getTeams()
+    .pipe(take(1))
+    .subscribe(teams => {
+      if (teams.length > 0) {
+        this.team= teams[0]
+      }
+    })
+  }
+
+  private newPlayer(playerFormValue){
+    const key = this.playerService.addPlayer(playerFormValue).key;
+    const playerFormValueKey ={
+      ...playerFormValue,
+      key
+    }
+    const formattedTeam ={
+      ... this.team,
+      players: [...(this.team.players ? this.team.players : []),playerFormValue]
+    }
+    this.teamService.editTeam(formattedTeam);
+  }
+
+  onSubmit(playerForm: NgForm){
+    const playerFormValue={...playerForm.value}
+    if (playerForm.valid) {
+      playerFormValue.leftFooted = playerFormValue.leftFooted === '' ? false : playerFormValue.leftFooted;
+      this.newPlayer(playerFormValue);
+      window.location.replace('#');
+    }
+  }
+
+}
