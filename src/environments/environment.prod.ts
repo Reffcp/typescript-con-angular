a692d98657effb750f1446d64d2ad3f90bc4ee00
daff <daffmg1@gmail.com>
2020-08-07 07:05:08 -0500

    config firebase on production mode

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:  {
+    apiKey: "AIzaSyC6Y-yc8ppk8DpoQXwxB0GfudErDfEjqK4",
+    authDomain: "typescript-con-angular.firebaseapp.com",
+    databaseURL: "https://typescript-con-angular.firebaseio.com",
+    projectId: "typescript-con-angular",
+    storageBucket: "typescript-con-angular.appspot.com",
+    messagingSenderId: "1056407920592",
+    appId: "1:1056407920592:web:cd8cf82c7c29e17c3fe3d5"
+  }
 };
